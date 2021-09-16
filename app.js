2c014fd2a3abdcc48d5456c84779b3e053791701
rasel <russelroy15@gmail.com>
2021-09-16 15:46:57 +0600

    WIP on locat and session storage

--- app.js
+++ app.js
@@ -21,3 +21,27 @@ const displayProduct = name => {
     li.innerText = name;
     ul.appendChild(li);
 }
+
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+
+    if (cart) {  //aga thaka sava kora thaka
+        cartObj = JSON.parse(cart);
+    } else {
+        cartObj = [];
+    }
+    return cartObj;
+}
+
+const addProductToCart = name => {
+    console.log(name);
+    const cart = getCart;
+
+
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    } else {
+        cart[name] = 1;
+    }
+}
