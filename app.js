e048984f8e7d6e31473f4c9cd68b823cc160bc9d
rasel <russelroy15@gmail.com>
2021-09-16 15:39:58 +0600

    WIP on locat and session storage

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Checking js");
+// console.log("Checking js");
+const displayLocalStorage = () => {
+    const cart 
+}
+
+
+const addItem = () => {
+    const nameField = document.getElementById('product-name');
+    const name = nameField.value;
+    if (!name) {
+        return;
+    }
+    //display in the ul
+    
+}
+
+
+const displayProduct = name => {
+    const ul = document.getElementById('products');
+    const li = document.createElement('li');
+    li.innerText = name;
+    ul.appendChild(li);
+}
